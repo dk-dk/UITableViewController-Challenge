9c9d68cbdad0b6c0adbb66e4e8073f8c2ae1e455
Mac <Mac@Host.local>
2014-10-16 12:28:50 +0200

    Initial Commit

---
+++ UITableViewController Challenge/DMFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DMFAppDelegate.h
+//  UITableViewController Challenge
+//
+//  Created by Mac on 16/10/14.
+//  Copyright (c) 2014 Dani Mar Family. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DMFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewController Challenge/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UITableViewController Challenge
+//
+//  Created by Mac on 16/10/14.
+//  Copyright (c) 2014 Dani Mar Family. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DMFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DMFAppDelegate class]));
+    }
+}
